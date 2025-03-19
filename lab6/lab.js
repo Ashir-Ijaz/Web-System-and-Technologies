50e6cc77d147254a0164c14a9a4606cf2e004fe2
Ashir Ijaz <aashirijazsatti@gmail.com>
2025-03-20 01:03:38 +0500

    COMMIT

---
+++ lab6/lab.js
@@ -0,0 +1,5 @@
+
+console.log("Name: Ashir Ijaz");
+console.log("University: Bahria University, Islamabad Campus");
+console.log("Degree: Bachelor in Information Technology (BSIT)");
+console.log("Occupation: Developer");
